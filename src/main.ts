044f288667a0aece7bd58b3f2cd0c8369a1df1b9
PauloCMoreira <pauloskx9004@gmail.com>
2022-10-20 15:01:04 -0300

    update: Create Main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -8,6 +9,8 @@ async function bootstrap() {
 
   const port = 4000
 
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
